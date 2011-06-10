af948e011736414d97c3b3688ab617e8b281b649
Nowak Judyta <nowoczki@o2.pl>
2011-06-10 03:11:45 -0700

    Edited kolo1.cpp via GitHub

--- kolo1.cpp
+++ kolo1.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 void pytanie() {
 cout<<"Q1) Jak wyswietlic roznice miedzy drugim a trzecim zatwierdzeniem wykonanym w galezi master?"<<endl;
-cout<<"A1) git diff HEAD~2 HEAD~3"<<endl;
+cout<<"A1) git diff 9952 5672 bądź nie znając commita git diff HEAD~2 HEAD~3"<<endl;
 cout<<"Q2) Co sie zmienilo?"<<endl;
-cout<<"A2) Nic się nie zmieniło"<<endl;
+cout<<"A2) zmienila sie tresc int maina "<<endl;
 }
 
  int main(int argc, char **argv) {
